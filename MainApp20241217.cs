aa4722cfd896ce73850bebcc6ac74e5001ddee0d
minjungy <7700888@naver.com>
2024-12-17 14:08:24 +0900

    Add files via upload

---
+++ MainApp20241217.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        //프로그램 실행이 시작되는 곳
+        static void Mai(string[] args)
+        {
+            if ()
+        
+        
+        
+        
+        
+        }           
+    
+    
+    }
+}
